f7df617fe0964f283a9374ac31ac2e9860dc1d95
k1504262 <k1504262@kcl.ac.uk>
2017-11-29 01:22:16 +0000

    complete --add current balance func.

--- Portfolio.java
+++ Portfolio.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Portfolio extends ArrayList<InvestmentAnalysisSystem>{
+public class Portfolio extends ArrayList<InvestmentAnalysisSystem> {
 
 
     //investor wants to check the actual value of a bond under different inflation rate
@@ -9,45 +9,41 @@ public class Portfolio extends ArrayList<InvestmentAnalysisSystem>{
 
     public Portfolio(double r) {
 
-        this.r =r;
+        this.r = r;
 
 
     }
 
 
 
-    public void print(){
+    public void print(int i) {
 
 
-
-        for (int i = 0; i < this.size(); i++) {
-
-            System.out.println("<!-----------------------"+this.get(i).getInvestorName()+" "+this.get(i).getBondName()+" Analysis----------------------->\n");
+            System.out.println("<!-----------------------" + this.get(i).getInvestorName() + " " + this.get(i).getBondName() + " Analysis----------------------->\n");
 
 
             System.out.println(
-                            "investor: "+this.get(i).getInvestorName()+ "\n"+
+                            "name: " + this.get(i).getInvestorName() + "\n" +
+                            "current balance: " + this.get(i).getInvestorBalance() + "\n" +
                             "payout: " + this.get(i).payout() + "\n" +
-                            "r: " +r+ "\n" +
-                            "value("+r+"): " +this.get(i).value(r) + "\n" +
-                            "macaulayDuration("+r+"): " + this.get(i).macaulayDuration(r) + "\n"
+                            "r: " + r + "\n" +
+                            "value(" + r + "): " + this.get(i).value(r) + "\n" +
+                            "macaulayDuration(" + r + "): " + this.get(i).macaulayDuration(r) + "\n"+
+                            "irr: " + this.get(i).calculateIrr() + "\n"
             );
 
-            System.out.println(
-                            "<!--Delete me before submission fixed values for r to check for testcase--> "+"\n\n"+
+            System.out.println("# of bonds : "+this.get(i).getBondPurchasedNumber());
+
+          /*  System.out.println(
+                    "<!--Fixed value for r = 0.02 to check testcase--> " + "\n\n" +
                             "r: 0.02 \n" +
-                            "value(0.02): " +this.get(i).value(0.02) + "\n" +
-                            "macaulayDuration(0.02): " + this.get(i).macaulayDuration(0.02) + "\n\n"+
-                             "<!--Delete me before submission fixed values for r to check for testcase-->\n "
+                            "value(0.02): " + this.get(i).value(0.02) + "\n" +
+                            "macaulayDuration(0.02): " + this.get(i).macaulayDuration(0.02) + "\n" +
+                            "irr: " + this.get(i).calculateIrr() + "\n\n"+
+                    "<!--Fixed value for r = 0.02 to check testcase-->\n "
 
             );
-            System.out.println("<!-----------------------"+this.get(i).getInvestorName()+" "+this.get(i).getBondName()+" Analysis----------------------->\n");
-
-
-
-        }
-
-
+*/
 
 
 
